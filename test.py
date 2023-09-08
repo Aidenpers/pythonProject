037f902d8bc367c70076bd0639bd496ea3086ccf
Aidenpers <743868237@qq.com>
2023-09-08 09:21:32 +0800

    1

---
+++ test.py
@@ -0,0 +1,9 @@
+var1 = 123456
+var2 = 10
+print(var1,var2)
+str = ('Aidenpers','wacao')
+print(str[2:5])
+list = ['aidenpers','aaaaaaa','sakura']
+print(list[2])
+list [1]= 'wocao'
+print(list)
---
+++ test2.py
@@ -0,0 +1,11 @@
+dict = {}
+dict['one'] = "This is one"
+dict[2] = "This is two"
+
+tinydict = {'name': 'runoob', 'code': 6734, 'dept': 'sales'}
+
+print(dict['one'])  # 输出键为'one' 的值
+print(dict[2])
+print(tinydict)
+print(tinydict.keys())
+print(tinydict.values())
